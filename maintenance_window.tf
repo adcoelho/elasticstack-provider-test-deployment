88176cba2fc1b77466714f4ecf5534191cd4ffd1
adcoelho <antonio.coelho@elastic.co>
2025-09-05 15:16:51 +0200

    mw all fields

--- maintenance_window.tf
+++ maintenance_window.tf
@@ -1,20 +1,23 @@
 resource "elasticstack_kibana_maintenance_window" "my_maintenance_window" {
-  title   = "Terraform Maintenance Window Updated"
+  title   = "New Maintenance Window"
   enabled = true
-  custom_schedule {
-    start    = "1992-01-01T05:00:00.200Z"
-    duration = "12d"
 
-    recurring {
-      every       = "20d"
-      end         = "2029-05-17T05:05:00.000Z"
-      on_week_day = ["MO", "TU"]
+  custom_schedule = {
+    start    = "1993-01-01T05:00:00.200Z"
+    duration = "24h"
+
+    recurring = {
+      every        = "22d"
+      end          = "2029-05-17T05:05:40.000Z"
+      on_week_day  = ["MO", "+2TU", "-2FR"]
+      on_month_day = [1, 2, 4, 5, 8]
+      on_month     = [12]
     }
   }
 
-  scope {
-    alerting {
-      kql = "_id: '1234'"
+  scope = {
+    alerting = {
+      kql = "_id: '12345'"
     }
   }
 }
